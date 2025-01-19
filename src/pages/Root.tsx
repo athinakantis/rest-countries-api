159b23be9a9ddb916c6dfe6e3f54eb0beb87c1c5
Athina <athina.kantis@gmail.com>
2025-01-19 14:45:48 +0200

    update Root.tsx

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 
+
 function Root() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <>
+    <div id='ThemeProvider'>
       <Header />
-      <main>
+      <main id={theme}>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
