d58c439a9dbea281d946008d0d79eb94d3295454
Athina <athina.kantis@gmail.com>
2025-01-19 14:44:13 +0200

    update requests, types

--- src/utils/requests.tsx
+++ src/utils/requests.tsx
@@ -1,23 +1,44 @@
-import axios from 'axios'
-import { Country } from '../services/types'
+import axios from 'axios';
+import { Country, CountrySimplified, BorderCountry } from '../services/types';
 
-export const fetchAllCountries = async (): Promise<Country[]> => {
-  const response = await axios.get<Country[]>('https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags,cca3')
-  console.log(response.data);
+export const fetchAllCountries = async (): Promise<CountrySimplified[]> => {
+  const response = await axios.get<CountrySimplified[]>(
+    'https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags'
+  );
   return response.data;
-}
+};
 
 export const fetchOneCountry = async (name: string): Promise<Country> => {
-  const response = await axios.get<Country[]>(`https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags`)
-  return response.data[0]
+  const response = await axios.get<Country[]>(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags`
+  );
+  return response.data[0];
+};
+
+export function filterCountries(
+  searchterm: string,
+  countries: CountrySimplified[]
+): CountrySimplified[] {
+  return countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchterm.toLowerCase())
+  );
 }
 
-export function filterCountries(searchterm: string, countries: Country[]): Country[] {
-  return countries.filter(country => country.name.common.toLowerCase().includes(searchterm.toLowerCase()))
+export async function getBorderCountries(
+  borderCountries: string[]
+): Promise<string[]> {
+  const response = await axios.get<BorderCountry[]>(
+    `https://restcountries.com/v3.1/alpha?codes=${borderCountries.join(
+      ','
+    )}&fields=name`
+  );
+
+  return response.data.map((country) => country.name.common);
 }
 
-export async function getBorderCountries(borderCountries: string[]): Promise<string[]> {
-  const response = await axios.get<string[]>(`https://restcountries.com/v3.1/alpha?codes=${borderCountries.join(',')}&fields=name`)
-
-  return response.data.map(country => country.name.common)
-}
+export const fetchByRegion = async (region: string): Promise<CountrySimplified[]> => {
+  const response = await axios.get<CountrySimplified[]>(
+    `https://restcountries.com/v3.1/region/${region}?fields=name,population,capital,region,flags`
+  );
+  return response.data;
+};
