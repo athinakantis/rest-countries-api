2472bfe99beac4afaf1acd2fb2db69f918d52a06
Athina <athina.kantis@gmail.com>
2025-01-19 14:46:31 +0200

    update SinglePage.tsx: add data and format functions

--- src/pages/SinglePage.tsx
+++ src/pages/SinglePage.tsx
@@ -1,7 +1,8 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { fetchOneCountry, getBorderCountries } from '../utils/requests';
 import { useEffect, useState } from 'react';
 import { Country } from '../services/types';
+import { formatCurrencies, formatLanguages, formatTLD } from '../utils/formatData';
 
 function SinglePage() {
   const { name } = useParams<string>();
@@ -10,26 +11,28 @@ function SinglePage() {
   useEffect(() => {
     if (!name) return;
 
-    fetchOneCountry(name)
-      .then((data) => {
-        setCountry(data);
+    const getCountry = async (name: string) => {
+      const data = await fetchOneCountry(name);
+      setCountry(data);
 
-        return getBorderCountries(data.borders);
-      })
-      .then((dataB) => {
-        setCountry((prev) =>
-          prev ? { ...prev, borders: dataB } : prev
-        );
-      })
-  }, []);
-
-
+      if (data.borders.length > 0) {
+        getBorderCountries(data.borders).then((dataB) => {
+          setCountry((prev) => (prev ? { ...prev, borders: dataB } : prev));
+        });
+      }
+    };
+    getCountry(name);
+  }, [name]);
 
   return (
     <div className='single-page-country-container'>
-      <button id='back'>Back</button>
+      <Link
+        to='/'
+        id='back'
+      >
+        Back
+      </Link>
       <div className='single-page-country'>
-
         {country && (
           <>
             <img
@@ -38,11 +41,29 @@ function SinglePage() {
             />
             <div className='country-details-container'>
               <h4>{country.name.common}</h4>
-              <div className='country-details'></div>
-              <div className="border-countries">
-                <span>Border Countries: </span>
-                {country.borders?.map(borderCountry => <button key={borderCountry}>{borderCountry}</button>)}
+              <div className='country-details'>
+                <div className="country-details-1">
+                  <p><span>Native Name: </span>{(Object.values(country.name.nativeName)[0]).common}</p>
+                  <p><span>Population: </span>{country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                  <p><span>Region: </span>{country.region}</p>
+                  <p><span>Sub Region: </span>{country.subregion}</p>
+                  <p><span>Capital: </span>{country.capital.join(', ')}</p>
+                </div>
+                <div className="country-details-2">
+                  {formatTLD(country.tld)}
+                  {formatCurrencies(country.currencies)}
+                  {formatLanguages(country.languages)}
+                </div>
+
               </div>
+              {country.borders.length > 0 && (
+                <div className='border-countries'>
+                  <span>Border Countries: </span>
+                  {country.borders.map((borderCountry) => (
+                    <Link to={`/${borderCountry}`}>{borderCountry}</Link>
+                  ))}
+                </div>
+              )}
             </div>
           </>
         )}
