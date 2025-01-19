a865d1995afeea609fd382358398b353edc8c181
Athina <athina.kantis@gmail.com>
2025-01-19 14:44:38 +0200

    update types.tsx: add types

--- src/services/types.tsx
+++ src/services/types.tsx
@@ -3,25 +3,68 @@ export interface Country {
   id: Country['cca3'];
   name: {
     common: string;
-  }
-  languages?: object;
-  currencies?: object;
-  tld?: string[];
-  capital?: string[];
-  population?: number;
-  subregion?: string;
-  region?: string;
+    nativeName: NativeName
+  };
+  languages: object;
+  currencies: Currency;
+  tld: string[];
+  capital: string[];
+  population: number;
+  subregion: string;
+  region: string;
   flags: {
     png: string;
     svg: string;
-  }
-  borders: string[]
+  };
+  borders: string[];
 }
 
+interface NativeName {
+  language: {
+    official: string;
+    common: string;
+  }
+}
 
+export interface Currency {
+  currency: {
+    name: string;
+    symbol: string;
+  }
+}
+
+export interface CountrySimplified {
+  name: {
+    common: string;
+  };
+  population: number;
+  capital: string[];
+  region: string;
+  flags: {
+    png: string;
+    svg: string;
+  },
+  cca3: string;
+  id: CountrySimplified['cca3'],
+  borders: string[];
+}
+
+export interface BorderCountry {
+  name: {
+    common: string;
+  }
+}
+
+export type Theme = 'light' | 'dark';
 
 export interface SearchBarProps {
   setSearchterm: (searchterm: string) => void;
 }
 
-export type Filter = 'Filter by region' | 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+export type Filter =
+  | 'Filter by region'
+  | 'Africa'
+  | 'Americas'
+  | 'Asia'
+  | 'Europe'
+  | 'Oceania';
