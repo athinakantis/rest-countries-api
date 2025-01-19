d2c25a35e57dfdd3cc04a7ca225fd81862b8e71e
Athina <athina.kantis@gmail.com>
2025-01-19 14:42:45 +0200

    add data: themes and filters

---
+++ src/utils/data.tsx
@@ -0,0 +1,14 @@
+export const filters = ['Filter by region', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+export const themes = {
+  "light": {
+    "--background": 'hsl(0, 0%, 98%)',
+    '--elements': 'hsl(0, 0%, 100%)',
+    '--text': 'hsl(207, 26%, 17%)'
+  },
+  "dark": {
+    '--background': 'hsl(207, 26%, 17%)',
+    '--elements': 'hsl(209, 23%, 22%)',
+    '--text': 'hsl(0, 0%, 100%)'
+  }
+}
