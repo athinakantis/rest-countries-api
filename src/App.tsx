ae73989a7a83cdab9813f8769665a8a730f5d705
Athina <athina.kantis@gmail.com>
2025-01-19 14:43:33 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { createContext, useState } from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './utils/router'
-export const ThemeContext : any = createContext(null)
+import { createContext, useState, useEffect } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './utils/router';
+import { Theme } from './services/types';
+import { themes } from './utils/data';
 
-function App() {
-  const [theme, setTheme] = useState<string>(JSON.parse(localStorage.getItem('theme')) || 'light')
-
-  return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
-      <RouterProvider router={router} />
-    </ThemeContext.Provider>
-  )
+interface ThemeContextType {
+  theme: Theme;
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
 }
 
-export default App
+export const ThemeContext = createContext<ThemeContextType | null>(null);
+
+
+function App() {
+  const [theme, setTheme] = useState<Theme>(
+    (JSON.parse(localStorage.getItem('theme') as string) as Theme) || 'light'
+  );
+  const root = document.querySelector('html') as HTMLElement;
+
+  useEffect(() => {
+    for (const variable in themes[theme]) {
+      root.style.setProperty(variable, themes[theme][variable])
+    }
+    localStorage.setItem('theme', JSON.stringify(theme))
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <RouterProvider router={router} />
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
