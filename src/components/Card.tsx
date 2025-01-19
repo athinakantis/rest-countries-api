4339788da2e342601cafff4876656c206bb75a0f
Athina <athina.kantis@gmail.com>
2025-01-19 14:44:56 +0200

    update Card component, types and styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,20 @@
-import { Country } from '../services/types';
+import { Link } from 'react-router-dom';
+import { CountrySimplified } from '../services/types';
 import './Card.css'
 
-function Card({ country }: { country: Country }) {
+function Card({ country }: { country: CountrySimplified }) {
   const { name, flags, population, region, capital } = country
 
 
   return (
     <div className='country-card'>
-      <img src={flags.png} alt={name.common} />
+      <Link to={`/${country.name.common}`}>
+        <img src={flags.png} alt={name.common} />
+      </Link>
       <div className='country-details'>
 
         <h4>{name.common}</h4>
-        <p><span>Population: </span>{population?.toLocaleString().replace('&nbsp', ',')}</p>
+        <p><span>Population: </span>{population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
         <p><span>Region: </span>{region}</p>
         <p><span>Capital: </span>{capital.length > 1 ? capital.join(', ') : capital[0]}</p>
       </div>
