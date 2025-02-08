34de437d55c9dfa90d919efb6e2099833efa3980
Athina <athina.kantis@gmail.com>
2025-02-08 20:24:48 +0200

    update requests to use createAsyncThunk

--- src/utils/requests.tsx
+++ src/utils/requests.tsx
@@ -1,44 +1,46 @@
 import axios from 'axios';
-import { Country, CountrySimplified, BorderCountry } from '../services/types';
+import { Country, BorderCountry } from '../services/types';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchAllCountries = async (): Promise<CountrySimplified[]> => {
-  const response = await axios.get<CountrySimplified[]>(
-    'https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags'
-  );
-  return response.data;
-};
 
-export const fetchOneCountry = async (name: string): Promise<Country> => {
-  const response = await axios.get<Country[]>(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags`
-  );
-  return response.data[0];
-};
+export const fetchAllCountries = createAsyncThunk(
+  '/countries/fetchAllCountries',
+  async () => {
+    const response = await axios.get<Partial<Country>[]>(
+      'https://restcountries.com/v3.1/all?fields=name,population,capital,region,flags'
+    );
+    return response.data;
+  }
+);
 
-export function filterCountries(
-  searchterm: string,
-  countries: CountrySimplified[]
-): CountrySimplified[] {
-  return countries.filter((country) =>
-    country.name.common.toLowerCase().includes(searchterm.toLowerCase())
-  );
-}
+export const fetchRegionalCountries = createAsyncThunk(
+  '/countries/fetchRegionalCountries',
+  async (region: string) => {
+    const response = await axios.get<Partial<Country>[]>(
+      `https://restcountries.com/v3.1/region/${region}?fields=name,population,capital,region,flags`
+    );
+    return response.data;
+  }
+);
 
-export async function getBorderCountries(
-  borderCountries: string[]
-): Promise<string[]> {
-  const response = await axios.get<BorderCountry[]>(
-    `https://restcountries.com/v3.1/alpha?codes=${borderCountries.join(
-      ','
-    )}&fields=name`
-  );
+export const fetchOneCountry = createAsyncThunk(
+  '/countries/fetchOneCountry',
+  async (name: string) => {
+    const response = await axios.get<Country[]>(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,population,capital,region,tld,subregion,currencies,languages,borders,cca3,flags`
+    );
+    return response.data[0];
+  }
+);
 
-  return response.data.map((country) => country.name.common);
-}
-
-export const fetchByRegion = async (region: string): Promise<CountrySimplified[]> => {
-  const response = await axios.get<CountrySimplified[]>(
-    `https://restcountries.com/v3.1/region/${region}?fields=name,population,capital,region,flags`
-  );
-  return response.data;
-};
+export const fetchBorderCountries = createAsyncThunk(
+  '/countries/fetchBorderCountries',
+  async (borderCountries: string[]): Promise<string[]> => {
+    const response = await axios.get<BorderCountry[]>(
+      `https://restcountries.com/v3.1/alpha?codes=${borderCountries.join(
+        ','
+      )}&fields=name`
+    );
+    return response.data.map((country) => country.name.common);
+  }
+);
