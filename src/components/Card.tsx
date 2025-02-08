5b0537b2b562ccbff15d39017d47c5020a9c02ab
Athina <athina.kantis@gmail.com>
2025-02-08 20:25:57 +0200

    update country Type

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,22 @@
 import { Link } from 'react-router-dom';
-import { CountrySimplified } from '../services/types';
+import { Country } from '../services/types';
 import './Card.css'
 
-function Card({ country }: { country: CountrySimplified }) {
+function Card({ country }: { country: Partial<Country> }) {
   const { name, flags, population, region, capital } = country
 
 
   return (
     <div className='country-card'>
-      <Link to={`/${country.name.common}`}>
-        <img src={flags.png} alt={name.common} />
+      <Link to={`/${country!.name!.common}`}>
+        <img src={flags!.png} alt={name!.common} />
       </Link>
       <div className='country-details'>
 
-        <h4>{name.common}</h4>
-        <p><span>Population: </span>{population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        <h4>{name!.common}</h4>
+        <p><span>Population: </span>{population!.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
         <p><span>Region: </span>{region}</p>
-        <p><span>Capital: </span>{capital.length > 1 ? capital.join(', ') : capital[0]}</p>
+        <p><span>Capital: </span>{capital!.length > 1 ? capital!.join(', ') : capital![0]}</p>
       </div>
     </div>
   )
